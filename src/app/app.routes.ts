74a757bc80be33c6b2ac8653ed66bee10ed38b2f
delauwv <delauwvalentin@gmail.com>
2025-03-28 17:54:08 +0100

    Ajout de la route pour l'inscription et redirection automatique vers /login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './routes/auth/login/login.component';
+import { SignupComponent } from './routes/auth/signup/signup.component';
 
-export const routes: Routes = [{ path: 'login', component: LoginComponent }];
+export const routes: Routes = [
+    { path: '',   redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent }, 
+    { path: 'signup', component: SignupComponent },
+];
