55954bc6fea93440781bbf6f2aa72e590c85b612
delauwv <delauwvalentin@gmail.com>
2025-03-28 17:57:51 +0100

    Ajout de la méthode d'inscription

--- src/app/routes/auth/auth.service.ts
+++ src/app/routes/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@/environments/environment';
 import { LoginRequest } from '@/app/models/loginRequest.interface';
+import { SignupRequest } from '@/app/models/signupRequest.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,10 @@ export class AuthService {
   private http = inject(HttpClient);
 
   login(data: LoginRequest) {
-    return this.http.post(`${environment.apiBaseUrl}`, data);
+    return this.http.post(`${environment.apiBaseUrl}/auth/login`, data);
+  }
+
+  signup(data: SignupRequest) {
+    return this.http.post(`${environment.apiBaseUrl}/auth/signup`, data);
   }
 }
