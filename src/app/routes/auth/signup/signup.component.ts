604974c33c9a76e9720030c971eb2ef4ec04146c
delauwv <delauwvalentin@gmail.com>
2025-03-28 17:58:22 +0100

    Création de l'interface pour la page d'inscription

--- src/app/routes/auth/signup/signup.component.ts
+++ src/app/routes/auth/signup/signup.component.ts
@@ -1,9 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { passwordMatchValidator } from '@app/validators/passwordMatchValidator';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
 })
-export class SignupComponent {}
+export class SignupComponent implements OnInit {
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  signupForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group(
+      {
+        firtsname: ['', [Validators.required]],
+        lastname: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validators: passwordMatchValidator('password', 'confirmPassword')
+      }
+    );
+  }
+
+  onSubmit(): void {
+    this.signupForm.markAllAsTouched();
+
+    if (this.signupForm.valid) {
+      this.authService.login(this.signupForm.value).subscribe({
+        next: (response: any) => {
+          console.log(response)
+        },
+        error : (error: any) => {
+          console.error(error)
+        }
+      }); 
+    } else {
+      this.signupForm.markAllAsTouched();
+    }
+  }
+
+  toLogin(){
+    this.router.navigate(['/login'])
+  }
+}
