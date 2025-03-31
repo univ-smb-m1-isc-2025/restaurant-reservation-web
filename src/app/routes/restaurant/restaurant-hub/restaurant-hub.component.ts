3c399664cd413d0a49ceeeddd38cadcf6e8e0e26
delauwv <delauwvalentin@gmail.com>
2025-03-31 14:57:30 +0200

    Début de la page de la liste des restaurants

---
+++ src/app/routes/restaurant/restaurant-hub/restaurant-hub.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'restaurant-hub',
+  standalone: true,
+  imports: [],
+  templateUrl: './restaurant-hub.component.html',
+})
+export class RestaurantHubComponent {
+
+}
