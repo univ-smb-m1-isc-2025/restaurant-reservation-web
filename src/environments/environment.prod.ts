f5b06a44913ba084e229cce279fcc3c2c8380b04
delauwv <delauwvalentin@gmail.com>
2025-04-16 20:51:58 +0200

    Ajout d'une webBaseUrl dans environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://api.reza.oups.net',
+  webBaseUrl: 'https://www.reza.oups.net'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080',
+  webBaseUrl: 'http://localhost:4200'
 };
